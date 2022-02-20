661332df9cf09579ee58acca9060ee2c073161ef
XanderJB <kurisekun@gmail.com>
2022-02-20 17:57:34 +0300

    исправил баг с делением на 0

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -2,6 +2,11 @@ while True:
     a = int(input("Введите 1-е число: "))
     b = int(input("Введите 2-е число: "))
     res = input("Выберите действие: \n1 - сложение; \n2 - вычитание; \n3 - умножение; \n4 - деление.\n")
+    try :
+        (res == a // b)
+    except ZeroDivisionError :
+        print("Делить на 0 нельзя!\nОстановка выполнения программы")
+        break
     def multi_num(res):
         if res == '1':
             return a + b
@@ -10,12 +15,13 @@ while True:
         if res == '3':
             return a * b
         if res == '4':
-            return a / b
+            return a // b; a/b
         raise ValueError('Неизвестный оператор {}'.format.res)
     print(multi_num(res))
     exit = input("Введите q, чтобы выйти. \nНажмите Enter, чтобы продолжить использовать Калькулятор: ")
     if exit == "q":
         break
+input("Нажмите любую клавишу, чтобы выйти...")
     
     
 
